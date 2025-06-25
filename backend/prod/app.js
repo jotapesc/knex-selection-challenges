fa332a0659428c89029b86529d19ad2cd4dcf7d7
João Pedro <jpsc081204@gmail.com>
2025-06-25 17:53:41 -0300

    feat: adiciona a rota upload-ceap do tipo POST

--- backend/prod/app.js
+++ backend/prod/app.js
@@ -5,9 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const fs_1 = __importDefault(require("fs"));
+const csv_parser_1 = __importDefault(require("csv-parser"));
+const multer_1 = __importDefault(require("multer"));
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const upload = (0, multer_1.default)({ dest: "uploads/" });
 app.use(express_1.default.json());
+app.post("/upload-ceap", upload.single("ceapFile"), (req, res) => {
+    if (!req.file) {
+        res.status(400).json({ error: "Nenhum arquivo CSV enviado." });
+    }
+    else {
+        const results = [];
+        const filePath = req.file.path;
+        fs_1.default.createReadStream(filePath)
+            .pipe((0, csv_parser_1.default)({ separator: ';' }))
+            .on("data", (data) => {
+            if (data.sgUF !== "NA") {
+                results.push(data);
+            }
+        })
+            .on("end", () => {
+            fs_1.default.unlinkSync(filePath);
+            res.json({ data: results });
+        })
+            .on("error", (error) => {
+            res.status(500).json({ error: `Erro ao processar o CSV: ${error.message}` });
+        });
+    }
+});
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,39 @@
 import express from "express";
 import "dotenv/config";
+import fs from "fs";
+import csv from "csv-parser";
+import multer from "multer";
 
 const app = express();
 const port = process.env.PORT;
+const upload = multer({ dest: "uploads/" });
 
 
 app.use(express.json());
 
+app.post("/upload-ceap", upload.single("ceapFile"), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ error: "Nenhum arquivo CSV enviado." });
+  } else {
+    const results: any[] = [];
+    const filePath = req.file.path;
 
+    fs.createReadStream(filePath)
+      .pipe(csv({ separator: ';' }))
+      .on("data", (data) => {
+        if (data.sgUF !== "NA") {
+          results.push(data);
+        }
+      })
+      .on("end", () => {
+        fs.unlinkSync(filePath);
+        res.json({ data: results });
+      })
+      .on("error", (error) => {
+        res.status(500).json({ error: `Erro ao processar o CSV: ${error.message}` });
+      });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
