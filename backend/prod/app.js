51fdc48abbce0fd89d1a762d382c749e317e9804
João Pedro <jpsc081204@gmail.com>
2025-06-26 17:50:19 -0300

    feat: adiciona async e Promise a function da rota upload-ceap

--- backend/prod/app.js
+++ backend/prod/app.js
@@ -16,27 +16,37 @@ app.get("/deputados", (req, res) => {
     const { uf } = req.query;
     //pesquisa todos os deputados com a uf indicada
 });
-app.post("/upload-ceap", upload.single("ceapFile"), (req, res) => {
+app.post("/upload-ceap", upload.single("ceapFile"), async (req, res) => {
     if (!req.file) {
         res.status(400).json({ error: "Nenhum arquivo CSV enviado." });
     }
     else {
         const results = [];
         const filePath = req.file.path;
-        fs_1.default.createReadStream(filePath)
-            .pipe((0, csv_parser_1.default)({ separator: ';' }))
-            .on("data", (data) => {
-            if (data.sgUF !== "NA") {
-                results.push(data);
+        try {
+            await new Promise((resolve, reject) => {
+                fs_1.default.createReadStream(filePath)
+                    .pipe((0, csv_parser_1.default)({ separator: ';' }))
+                    .on("data", (data) => {
+                    if (data.sgUF && data.sgUF !== "NA") {
+                        results.push(data);
+                    }
+                })
+                    .on("end", () => {
+                    fs_1.default.unlinkSync(filePath);
+                    resolve();
+                })
+                    .on("error", (error) => {
+                    reject(error);
+                });
+            });
+        }
+        catch (error) {
+            if (fs_1.default.existsSync(filePath)) {
+                fs_1.default.unlinkSync(filePath);
             }
-        })
-            .on("end", () => {
-            fs_1.default.unlinkSync(filePath);
-            res.json({ data: results });
-        })
-            .on("error", (error) => {
-            res.status(500).json({ error: `Erro ao processar o CSV: ${error.message}` });
-        });
+            res.status(500).json({ error: `Erro ao processar o CSV` });
+        }
     }
 });
 app.listen(port, () => {
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,29 +18,36 @@ app.get("/deputados", (req, res) => {
   //pesquisa todos os deputados com a uf indicada
 });
 
-app.post("/upload-ceap", upload.single("ceapFile"), (req, res) => {
+app.post("/upload-ceap", upload.single("ceapFile"), async (req, res) => {
   if (!req.file) {
     res.status(400).json({ error: "Nenhum arquivo CSV enviado." });
   } else {
     const results: any[] = [];
     const filePath = req.file.path;
 
-    fs.createReadStream(filePath)
+    try {
+      await new Promise<void>((resolve, reject) => {fs.createReadStream(filePath)
       .pipe(csv({ separator: ';' }))
       .on("data", (data) => {
-        if (data.sgUF !== "NA") {
+        if (data.sgUF && data.sgUF !== "NA") {
           results.push(data);
         }
       })
       .on("end", () => {
         fs.unlinkSync(filePath);
-        res.json({ data: results });
+        resolve();
       })
       .on("error", (error) => {
-        res.status(500).json({ error: `Erro ao processar o CSV: ${error.message}` });
-      });
-  }
-});
+        reject(error);
+        
+      });})
+    } catch (error) {
+      if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+    res.status(500).json({ error: `Erro ao processar o CSV` });
+    } 
+ } });
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
